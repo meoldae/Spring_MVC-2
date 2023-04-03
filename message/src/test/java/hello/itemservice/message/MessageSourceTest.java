0c713d45a46b51e44eaf69003684a98ab743824d
Junyoung <wnsdud12365@naver.com>
2023-04-03 23:08:27 +0900

    [Add]스프링 프로젝트에 메시지 적용

---
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,58 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class MessageSourceTest {
+    
+    // 자동으로 Resources 에 있는 파일 주입
+    @Autowired
+    MessageSource messageSource;
+
+    @Test
+    void helloMessage() {
+        String result = messageSource.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        // "no_code" 라는 메시지가 없으니 Exception 발생
+        assertThatThrownBy(() -> messageSource.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        // Default 메시지를 주면 Exception 발생하지 않음
+        String result = messageSource.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        // Argument 를 넘겨서 치환
+        // 단일값이라도 배열로 넘겨야 함
+        String result = messageSource.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(result).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang() {
+        assertThat(messageSource.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang() {
+        assertThat(messageSource.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+    }
+}
