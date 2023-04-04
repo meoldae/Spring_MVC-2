28cdbe3b151a167fd396acf8b85c65d341dc7a74
Junyoung <wnsdud12365@naver.com>
2023-04-04 22:35:26 +0900

    [Add] 검증 처리 구현

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -5,10 +5,13 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v1/items")
@@ -38,7 +41,37 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 결과 보관
+        Map<String, String> errors = new HashMap<>();
+
+        // 검증
+        if (!StringUtils.hasText(item.getItemName())){ // 넘어온 item Name이 없으면
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 10000000 ) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 원 까지 허용됩니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999 ){
+            errors.put("quantity", "재고는 9,999개 까지 허용됩니다.");
+        }
+
+        // 복합 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int result = item.getPrice() * item.getQuantity();
+            if (result < 10000) {
+                errors.put("global", "가격 * 수량의 합은 10,000 이상이어야 합니다. 현재 값 : " + result);
+            }
+        }
+
+        // 검증 실패 시 입력 폼으로 다시 보내버림
+        if (!errors.isEmpty()) { // 에러가 비어있지 않다면? 하나라도 에러가 있으면
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
+        // 검증 성공
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
